83a77fa3b4076c6a3081482f8caacbbb64b3252b
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-08 13:27:16 +0530

    edit interview page

--- src/app/(interview)/interview/page.tsx
+++ src/app/(interview)/interview/page.tsx
@@ -1,8 +1,27 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+
 export default function InterviewPage(){
 
+
     return(
-        <div>
-            Hi I am the Intevriew Page !!!
+        <div className="h-screen w-screen flex flex-col items-center py-14 space-y-10">
+
+            <div>
+                Welcome to the Interview page
+            </div>
+
+            <div className="flex flex-col items-center space-y-4">
+
+                <div>
+                    Tap the button to record the voice 
+                </div>
+                <div>
+                    <Button>Start</Button>
+                </div>
+
+            </div>
         </div>
     )
 }
