ded62eaf662a657d6999fc0f5860386474ea6bbe
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-08 20:08:59 +0530

    build interview page

--- src/app/(interview)/interview/page.tsx
+++ src/app/(interview)/interview/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
+import useRecognise from "@/hooks/useRecognise";
+import useRecording from "@/hooks/useRecording";
 
 export default function InterviewPage(){
 
-
+    // const {startRecording,stopRecording,isrecording} = useRecording();
+    const {startRecording, stopRecording, isRecording, transcription} = useRecognise();
     return(
         <div className="h-screen w-screen flex flex-col items-center py-14 space-y-10">
 
@@ -18,9 +21,34 @@ export default function InterviewPage(){
                     Tap the button to record the voice 
                 </div>
                 <div>
-                    <Button>Start</Button>
+                    <Button onClick={()=>{
+                        startRecording();
+                    }}>Start</Button>
                 </div>
 
+            </div>
+
+            <div className="flex flex-col items-center space-y-4">
+
+                <div>
+                    Tap the button to stop the voice 
+                </div>
+                <div>
+                    <Button onClick={()=>{
+                        stopRecording();
+                    }}>Stop</Button>
+                </div>
+
+            </div>
+
+            <div>
+                {isRecording ? `Your voice is being Recorded !!` : `The Recording is stopped !!`}
+            </div>
+
+            <div>
+                {
+                    `The Recorded Text is ${transcription}`
+                }
             </div>
         </div>
     )
