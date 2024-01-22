4bebf9693824954654c7a05c76e07ad22cac68e8
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-22 20:36:04 +0530

    design the page

--- src/app/(interview)/interview/page.tsx
+++ src/app/(interview)/interview/page.tsx
@@ -2,12 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import useRecognise from "@/hooks/useRecognise";
-import useRecording from "@/hooks/useRecording";
+import { useTextToSpeech } from "@/hooks/useTextToSpeech";
 
 export default function InterviewPage(){
 
-    // const {startRecording,stopRecording,isrecording:isRecording} = useRecording();
+    // Speech to text and text-to-speech hooks 
+
     const {startRecording, stopRecording, isRecording, transcription} = useRecognise();
+    const {speak} = useTextToSpeech();
+
+
+    const initialize = () => {
+        speak('I am not in danger, I am the danger')
+    }
+
     return(
         <div className="h-screen w-screen flex flex-col items-center py-14 space-y-10">
 
@@ -22,24 +30,11 @@ export default function InterviewPage(){
                 </div>
                 <div>
                     <Button onClick={()=>{
-                        startRecording();
+                        initialize();                        
                     }}>Start</Button>
                 </div>
 
             </div>
-
-            {/* <div className="flex flex-col items-center space-y-4">
-
-                <div>
-                    Tap the button to stop the voice 
-                </div>
-                <div>
-                    <Button onClick={()=>{
-                        stopRecording();
-                    }}>Stop</Button>
-                </div>
-
-            </div> */}
 
             <div>
                 {isRecording ? `Your voice is being Recorded !!` : `Your Voice is not recorded !!`}
