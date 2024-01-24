d15d1569f1e16cf593c921e79180fd3c9a94e2ef
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-24 12:07:13 +0530

    add prismaAdapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,16 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { randomBytes, randomUUID } from "crypto"
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
+
+  //Prisma Adapter
+  adapter: PrismaAdapter(prisma),
+
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,7 +19,37 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-}
+
+  secret: process.env.SECRET as string,
+
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    strategy: "jwt",
+  
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
+    
+    // The session token is usually either a random UUID or string, however if you
+    // need a more customized session token string, you can define your own generate function.
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex")
+    },
+
+    
+  }
+
+
+} as AuthOptions;
 
 export const handler = NextAuth(authOptions)
 export {handler as GET, handler as POST}
