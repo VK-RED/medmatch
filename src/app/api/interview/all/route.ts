ed4cb826f4abf0c2bccfd7faaf4009218ec52c86
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-29 13:29:38 +0530

    get all interviews for an user

---
+++ src/app/api/interview/all/route.ts
@@ -0,0 +1,44 @@
+import { SOMETHING_WENT_WRONG, USER_NOT_EXISTS, USER_NOT_LOGGED_IN } from "@/lib/constants";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions, prisma } from "../../auth/[...nextauth]/route";
+
+
+export async function GET(req:NextRequest){
+
+    try {
+        
+        const session = await getServerSession(authOptions);
+        if(!session || !session.user || !session.user.email) return NextResponse.json({message:USER_NOT_LOGGED_IN});
+
+        const email = session.user.email;
+
+        const user = await prisma.user.findFirst({
+            where:{
+                email
+            },
+            select:{
+                chats:{
+                    orderBy:{
+                        createdAt:"asc"
+                    },
+                    select:{
+                        id:true,
+                        title:true,
+                    }
+                },
+                id:true,
+            }
+        })
+
+        if(!user || !user.id) return NextResponse.json({message:USER_NOT_EXISTS});
+
+        const chats = user.chats;
+
+        return NextResponse.json({interviews:chats});
+
+    } catch (error) {
+        console.log(error);
+        throw new Error(SOMETHING_WENT_WRONG);
+    }
+}
