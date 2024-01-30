f08ce65d92f7901afa316bb7f8eb0fc1f5f278f0
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-30 13:08:37 +0530

    Add Navbar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import 'regenerator-runtime/runtime'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 import NextAuthProvider from './context/client-provider'
-
+import { Navbar } from '@/components/navbar'
+import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -25,7 +26,18 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider session={session}>
-          {children}
+          
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+            <Navbar>
+              {children}
+            </Navbar>
+          </ThemeProvider>
+          
         </NextAuthProvider>
       </body>
     </html>
