32808402d2b68ba4984440effed537a1264e7723
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-02 13:05:43 +0530

    remove background color

--- src/components/dark-mode-toggler.tsx
+++ src/components/dark-mode-toggler.tsx
@@ -17,7 +17,7 @@ export function DarkModeToggler() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild className="dark:bg-black cursor-pointer">
+      <DropdownMenuTrigger asChild className="cursor-pointer">
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -25,13 +25,13 @@ export function DarkModeToggler() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="dark:bg-black">
-        <DropdownMenuItem className="cursor-pointer dark:hover:bg-zinc-800" onClick={() => setTheme("light")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer dark:hover:bg-zinc-800" onClick={() => setTheme("dark")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer dark:hover:bg-zinc-800" onClick={() => setTheme("system")}>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
