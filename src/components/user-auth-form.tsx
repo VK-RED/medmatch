547e6cee785669821e382501976d52bd48a20813
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-06 09:57:38 +0530

    add Google icon instead of Github Icon

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -63,9 +63,9 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         {isLoading ? (
           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
         ) : (
-          <Icons.gitHub className="mr-2 h-4 w-4" />
+          <Icons.google className="mr-2 h-4 w-4" />
         )}{" "}
-        GitHub
+        Google
       </Button>
     </div>
   )
