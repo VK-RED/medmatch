2f7015a443b61b04005c37753f7ce73a58e50daa
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-06 09:07:05 +0530

    Add Login button in Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,22 +4,36 @@ import { NavBarProps } from "@/lib/types"
 import { DarkModeToggler } from "./dark-mode-toggler"
 import { AvatarToggler } from "./avatar-toggler"
 import { useSession } from "next-auth/react"
+import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
 export const Navbar = ({children}:NavBarProps) => {
 
     const {status} = useSession();
+    const router = useRouter();
 
     return (
-        <div className="flex justify-between pt-3 pb-2 px-5 min-w-[420px] sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="flex justify-between pt-3 pb-2 px-5 min-w-[420px] sticky top-0 z-50 w-full border-b-2 shadow-xl bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
 
-            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight ">
+            <h1 className="scroll-m-20 text-3xl font-bold tracking-tight ">
                 MedMatch
             </h1>
 
             <div  className="flex justify-center items-center space-x-4">
                 <DarkModeToggler />
                     {/* TODO - YOU NEED TO SHOW AVATAR TOGGLER ONLY IF A USER IS SIGNED IN */}
-                
-                <AvatarToggler />
+
+                {
+                    status === 'unauthenticated' ? (
+                        <Button onClick={()=>{
+                            router.push('/signin');
+                        }}>
+                            Login
+                        </Button>
+                    )
+
+                    : <AvatarToggler/>
+                }
+
             </div>
             
         </div>
