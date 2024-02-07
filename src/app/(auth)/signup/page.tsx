6543cfd47d3bb88de8f5839c43301db7e9008433
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-07 15:34:30 +0530

    Design Signup Page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from "next"
+import AuthPage from "@/components/authPage"
+import { AuthStatus } from "@/lib/types"
+import { SIGNUP_DESCRIPTION, SIGNUP_TITLE } from "@/lib/constants"
+
+export const metadata: Metadata = {
+  title: "Signup",
+  description: "Create a New Account",
+}
+
 export default function Signup(){
 
-    return(
-        <div>
-            I am the signup page
-        </div>
-    )
+    return (
+        <AuthPage authstatus={AuthStatus.Signup} title={SIGNUP_TITLE} description={SIGNUP_DESCRIPTION}/>
+      )
 }
