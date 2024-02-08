3023715f3749f973682578a3c6137bc12d587a8b
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-08 08:42:00 +0530

    fix: hidden property of authPage

--- src/components/authPage.tsx
+++ src/components/authPage.tsx
@@ -8,8 +8,7 @@ export default function AuthPage({title, description, authstatus}: AuthPageProps
 
   return (
     <>
-    {/* TODO : FIX THE HIDDEN PROPERTY */}
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href={authstatus === AuthStatus.Signin ? "/signup" : "/signin"}
           className={cn(
@@ -47,7 +46,7 @@ export default function AuthPage({title, description, authstatus}: AuthPageProps
           </div>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex flex-col justify-center space-y-6 w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 {title}
