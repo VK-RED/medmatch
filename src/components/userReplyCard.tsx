215462930a1e357bc73a3567299a44909f34747b
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-10 16:17:44 +0530

    render transcription

--- src/components/userReplyCard.tsx
+++ src/components/userReplyCard.tsx
@@ -1,11 +1,11 @@
 import { useSession } from "next-auth/react"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Badge } from "./ui/badge"
+import { UserReplyCardProps } from "@/lib/types";
 
-export const UserReplyCard = () => {
+export const UserReplyCard = ({transcription,isRecording}:UserReplyCardProps) => {
 
     const {data:session} = useSession();
-    const text = "Irunthuttu Po "
     const userImg = session?.user?.image || "";
 
     return (
@@ -16,7 +16,7 @@ export const UserReplyCard = () => {
             </Avatar>
             <div className="p-3 rounded-lg bg-gray-200 dark:bg-zinc-800 order-2 mx-4 min-h-[50px] flex-col">
                 <Badge className="tracking-tighter mt-1 mb-2">{session?.user?.name || "User"}</Badge>
-                <p className="mx-1">{text}</p>
+                <p className="mx-1">{transcription}</p>
             </div>
         </div>
     )
