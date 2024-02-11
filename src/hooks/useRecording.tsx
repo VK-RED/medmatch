b252c83c939a900094c7dff1996a79f77f7e2443
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-11 12:42:18 +0530

    create url for the recorded audio blob

--- src/hooks/useRecording.tsx
+++ src/hooks/useRecording.tsx
@@ -1,14 +1,11 @@
 import { useEffect, useRef, useState } from "react"
 
-
-//Since this hook is also dependent on SpeechRecognition, we tend to use the useRecognise Hook .
-
 const useRecording = () => {
 
     const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
     const [isrecording, setIsRecording] = useState(false);
+    const [audioUrl,setAudioUrl] = useState("");
     const chunks = useRef<Blob[]>([]);
-    const recognitionRef = useRef<SpeechRecognition>();
 
     // Get the stream to initialize the mediaRecorder
     const initializeRecorder = (stream:MediaStream) => {
@@ -26,31 +23,12 @@ const useRecording = () => {
         }
 
         recorder.onstop = () => {
-            const audioBlob = new Blob(chunks.current,{type:'audio/wav'});
-            console.log(`The chunks are -> `, chunks.current);
-            console.log('The audioBlob is .....' , audioBlob);
+            const audioBlob = new Blob(chunks.current,{type:'audio/mp3'});
+            const url = URL.createObjectURL(audioBlob);
+            setAudioUrl((u)=>url);
         }
 
         setMediaRecorder(recorder);
-
-        // Initialize recognition
-
-        recognitionRef.current = new webkitSpeechRecognition();
-        const recognition = recognitionRef.current;
-
-        recognition.lang = 'en-US';
-        recognition.continuous = true;
-
-        recognition.onresult = (e) => {
-            const lIndex = e.results.length - 1;
-            const isFinal = e.results[lIndex].isFinal;
-
-            if(isFinal){
-                console.log(`The Sound has been ended !!!`);
-                console.log(e.results[lIndex][0].transcript);
-                stopRecording();
-            }
-        }
 
     }
 
@@ -66,17 +44,15 @@ const useRecording = () => {
     //functions to start and stop recording
 
     const startRecording = () => {
-        if(mediaRecorder && recognitionRef.current){
+        if(mediaRecorder){
             mediaRecorder.start();
-            recognitionRef.current.start();
             setIsRecording((pr) => true);
         }
     }
 
     const stopRecording = () => {
-        if(mediaRecorder && recognitionRef.current){
+        if(mediaRecorder){
             mediaRecorder.stop();
-            recognitionRef.current.stop();
             setIsRecording((pr) => false);
         }
     }
@@ -85,6 +61,7 @@ const useRecording = () => {
         isrecording,
         startRecording,
         stopRecording,
+        audioUrl
     }
 
 }
