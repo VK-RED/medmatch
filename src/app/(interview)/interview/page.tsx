122e7a127d5b282eaab29477712d227bd4d301c2
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-16 14:09:16 +0530

    Design Interview Page

--- src/app/(interview)/interview/page.tsx
+++ src/app/(interview)/interview/page.tsx
@@ -1,56 +1,78 @@
 'use client'
 
+import { InterviewerCard } from "@/components/interviewerCard";
 import { Button } from "@/components/ui/button";
-import useRecognise from "@/hooks/useRecognise";
-import { useTextToSpeech } from "@/hooks/useTextToSpeech";
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useEffect } from "react";
+import useRecording from "@/hooks/useRecording";
+import { ChatResponse } from "@/lib/types";
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react";
+import { VoiceRecorder } from "@/components/voiceRecording"
 
 export default function InterviewPage(){
 
     // Speech to text and text-to-speech hooks 
-
-    const {startRecording, stopRecording, isRecording, transcription} = useRecognise();
-    const {speak} = useTextToSpeech();
     const { data: session } = useSession()
-    
+    const {startRecording, stopRecording, isrecording, formData} = useRecording();
+    const [iResponse,setIresponse] = useState("");
+
+    //handle the chatID
+    const chatId = "clso8pb0900021nn9l3sj1gtm";
+
     useEffect(()=>{
-        console.log(session,"INside page.tsx");
+        if(!session){
+            // handle this case route the user to landing page
+        }
     },[session])
 
-    const initialize = () => {
-        speak('I am not in danger, I am the danger')
+    async function chat(){
+        formData?.append("chatId",chatId);
+
+        const res = await fetch("/api/interview/chat",{
+            method:"POST",
+            body: formData,
+        })
+
+        const data:ChatResponse = await res.json();
+        setIresponse((p)=>data.message);
+        const audio = new Audio(data.audioUri);
+        audio.play();
+        console.log(data);
     }
 
+
     return(
-        <div className="h-screen w-screen flex flex-col items-center py-14 space-y-10">
+        <div className="h-[90vh] w-screen flex flex-col items-center justify-center">
+            
+            <div className="flex flex-col items-center py-14 space-y-10  justify-center">
+                <InterviewerCard text={iResponse}/>
 
-            <div>
-                Welcome to the Interview page
-            </div>
-
-            <div className="flex flex-col items-center space-y-4">
-
-                <div>
-                    Tap the button to record the voice 
-                </div>
-                <div>
-                    <Button onClick={()=>{
-                        initialize();                        
-                    }}>Start</Button>
-                </div>
-
-            </div>
-
-            <div>
-                {isRecording ? `Your voice is being Recorded !!` : `Your Voice is not recorded !!`}
-            </div>
-
-            <div>
-                {
-                    transcription   
+                {   isrecording &&
+                    <div>
+                        <VoiceRecorder />
+                    </div>
                 }
+
+
+                <div className="flex items-center space-x-4">
+
+                    <div>
+                        <Button onClick={()=>{
+                            if(!isrecording){
+                                startRecording();
+                                return;
+                            }
+                            stopRecording();
+                        }}>{isrecording ? "Stop" : "Start"}</Button>
+                    </div>
+
+                    {!isrecording && <div>
+                        <Button onClick={chat}>{'Send'}</Button>
+                    </div>}
+
+                </div>
+
             </div>
+
         </div>
     )
 }
