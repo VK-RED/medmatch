795ec8ed5e8db36e6b5eb053f214dec28b520bf4
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-16 14:06:54 +0530

    remove the stream clean in useRecording hook

--- src/hooks/useRecording.tsx
+++ src/hooks/useRecording.tsx
@@ -7,12 +7,9 @@ const useRecording = () => {
     const chunks = useRef<Blob[]>([]);
     const formRef = useRef<FormData|null>(null);
     const [audioBlob, setAudioBlob] = useState<Blob>()
-    const mediaStreamRef = useRef<MediaStream|null>(null);
-    const [initial,setInitial] = useState(true);
 
     // Get the stream to initialize the mediaRecorder
     const initializeRecorder = (stream:MediaStream) => {
-        mediaStreamRef.current = stream;
         const recorder = new MediaRecorder(stream);
 
         //Implement the interface
@@ -30,8 +27,6 @@ const useRecording = () => {
             const aBlob = new Blob(chunks.current,{type:'audio/mp3'});
             setAudioBlob((p) => aBlob);
             formRef.current?.append('audio',aBlob,'audio.mp3');
-            mediaStreamRef.current?.getTracks().forEach(track => {console.log(track); return track.stop()});
-            mediaStreamRef.current = null;
         }
 
         setMediaRecorder(recorder);
@@ -51,27 +46,9 @@ const useRecording = () => {
     //functions to start and stop recording
 
     const startRecording = () => {
-        if(initial && mediaRecorder){
+        if (mediaRecorder) {
             mediaRecorder.start();
-            setIsRecording((pr) => true);
-            setInitial((p)=>false)
-        }
-        else if(!initial && mediaRecorder){
-            navigator.mediaDevices.getUserMedia({audio:true})
-            .then(stream => {
-                initializeRecorder(stream);
-                if (mediaRecorder) {
-                    mediaRecorder.start();
-                    console.log("Media Recorder started")
-                    setIsRecording(true);
-                }
-            })
-            .catch(error => {
-                console.error('Error accessing media devices:', error);
-            });
-        }
-        else{
-            console.log("Something weird is happenning !!")
+            setIsRecording((p)=>true);
         }
     }
 
