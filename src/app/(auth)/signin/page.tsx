4b21d2a5c80f350eff0b2cfbf65232ef13a0dd2e
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-26 16:37:55 +0530

    add session and provider to signin page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,13 +2,19 @@ import { Metadata } from "next"
 import AuthPage from "@/components/authPage"
 import { AuthStatus } from "@/lib/types"
 import { SIGNIN_DESCRIPTION, SIGNIN_TITLE } from "@/lib/constants"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { getProviders } from "next-auth/react"
 
 export const metadata: Metadata = {
   title: "Signin",
   description: "Signin to your account",
 }
 
-export default function AuthenticationPage(){
+export default async function AuthenticationPage(){
+
+  const session = await getServerSession(authOptions);
+  const providers = await getProviders();
   
   return (
     <AuthPage authstatus={AuthStatus.Signin} title={SIGNIN_TITLE} description={SIGNIN_DESCRIPTION}/>
