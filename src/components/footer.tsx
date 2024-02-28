6f373fa018c3b6a3b017930e5fb61b419d72cc55
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-28 12:17:36 +0530

    Create Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import { Link } from "lucide-react"
+
+export const Footer = () => {
+    return (
+        <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Terms of Service
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Privacy
+          </Link>
+        </nav>
+      </footer>
+    )
+}
