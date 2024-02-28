5d0f9ea3ed9887519e109ebbd52e5995c1fc46ec
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-28 12:23:22 +0530

    Edit Footer Contents

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,17 @@
-import { Link } from "lucide-react"
+import Link from "next/link"
 
 export const Footer = () => {
     return (
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Medmatch Inc. All rights reserved.</p>
+        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
           </Link>
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Privacy
           </Link>
-        </nav>
+        </nav> */}
       </footer>
     )
 }
