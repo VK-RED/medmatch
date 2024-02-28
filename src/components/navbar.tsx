9957b12d190fadb43f039a7ce916ab0e6dbab923
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-28 11:22:50 +0530

    revamp navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,35 +4,45 @@ import { AvatarToggler } from "./avatar-toggler"
 import { useSession } from "next-auth/react"
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
+import Link from "next/link"
+import { MountainIcon } from "lucide-react"
 export const Navbar = () => {
 
     const {status} = useSession();
     const router = useRouter();
 
     return (
-        <div className="flex justify-between pt-3 pb-2 px-5 min-w-[400px] sticky top-0 z-50 w-full border-b-2 shadow-xl bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="px-4 lg:px-6 lg:py-6 h-14 flex items-center min-w-[400px] sticky top-0 z-50 w-full border-b-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <Link className="flex items-center justify-center" href="/">
+                <MountainIcon className="h-6 w-6" />
+                <span className="ml-3 font-bold"> Medmatch </span>
+            </Link>
 
-            <h1 className="scroll-m-20 text-3xl font-bold tracking-tight ">
-                MedMatch
-            </h1>
+            <nav className="ml-auto flex gap-4 sm:gap-6 items-center">
+                
+                <Link className="text-sm font-medium hover:underline underline-offset-4" href="/interview/all">
+                    History
+                </Link>
 
-            <div  className="flex justify-center items-center space-x-4">
-                <DarkModeToggler />
+                <div  className="flex justify-center items-center gap-4">
+                    <DarkModeToggler />
 
-                {
-                    status === 'unauthenticated' ? (
-                        <Button onClick={()=>{
-                            router.push('/signin');
-                        }}>
-                            Login
-                        </Button>
-                    )
+                    {
+                        status === 'unauthenticated' ? (
+                            <Button onClick={()=>{
+                                router.push('/signin');
+                            }}>
+                                Login
+                            </Button>
+                        )
 
-                    : <AvatarToggler/>
-                }
+                        : <AvatarToggler/>
+                    }
 
-            </div>
-            
-        </div>
+                </div>
+                
+            </nav>
+        </header>
     )
 }
+
