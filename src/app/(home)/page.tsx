bf9d926b55467ff2bd736c26026168854c0a25af
Vishnu Kumar <vishnukumart96@gmail.com>
2024-02-28 12:18:10 +0530

    Modularize the Landing Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,15 @@
-'use client'
-
-import Image from 'next/image'
+import { Features } from "@/components/features"
+import { Footer } from "@/components/footer"
+import { Herocard } from "@/components/heroCard"
 
 export default function Home() {
-
   return (
-    <div>
-      Welcome to Medmatch !!!
+    <div className="flex flex-col min-h-[100dvh]">
+      <main className="flex-1">
+        <Herocard />
+        <Features />
+      </main>
+      <Footer />
     </div>
   )
 }
