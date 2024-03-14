288de01e57e2c448eb8469a079d1cd665e6aa7a6
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-14 13:20:10 +0530

    fix min-width for userReplyCard

--- src/components/userReplyCard.tsx
+++ src/components/userReplyCard.tsx
@@ -7,8 +7,8 @@ export const UserReplyCard = ({transcription,session}:UserReplyCardProps) => {
     const userImg = session.user?.image || "";
 
     return (
-        <div className="flex items-end space-x-2 p-4 max-w-[350px] sm:max-w-[500px] lg:max-w-[700px] lg:min-w-[700px]">
-            <Avatar className="w-8 h-8 order-1 mr-2">
+        <div className="flex items-end space-x-2 p-4 max-w-[350px] sm:max-w-[500px] lg:max-w-[700px]">
+            <Avatar className="w-8 h-8 order-1">
                 <AvatarImage src={userImg} />
                 <AvatarFallback>{session?.user?.name?.substring(0,1) || 'U'}</AvatarFallback>
             </Avatar>
