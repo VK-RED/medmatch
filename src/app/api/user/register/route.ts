b01981e17f84a7e3b3ca139514121611b3df71b7
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-15 14:48:29 +0530

    Add Zod Validation for Register Endpoint

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -1,34 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import * as bcrypt from "bcryptjs"
+import { registerSchema } from "@/lib/types";
+import { ZodError } from "zod";
+import { USER_CREATED, USER_EXISTS_ALREADY } from "@/lib/constants";
 
 export async function POST(req:NextRequest){
 
-    //TODO: ADD ZOD VALIDATIONS
-    const {name,email,password} : {name:string, email:string, password:string} = await req.json();
 
-    const hashedPassword = bcrypt.hashSync(password,10);
+    try {
+        const body = await req.json();
+        const {name,email,password} = registerSchema.parse(body);
 
-    const existingUser = await prisma.user.findFirst({
-        where:{
-            email
-        }
-    })
+        const hashedPassword = bcrypt.hashSync(password,10);
 
-    if(!existingUser){
-        const user = await prisma.user.create({
-            data:{
-                email,
-                name,
-                password:hashedPassword,
+        const existingUser = await prisma.user.findFirst({
+            where:{
+                email
             }
         })
 
-        return NextResponse.json({message:"User Created Successfully !!!"});
+        if(!existingUser){
+            const user = await prisma.user.create({
+                data:{
+                    email,
+                    name,
+                    password:hashedPassword,
+                }
+            })
+
+            return NextResponse.json({message:USER_CREATED});
+        }
+        else{
+            throw new Error("User Exists Already");
+        } 
+    } catch (error) {
+        if(error instanceof ZodError){
+            console.log(error.message);
+            const message = `${error.issues[0].message}, Enter Valid ${error.issues[0].path}`
+            return NextResponse.json({message});
+        }
+        else{
+            console.log(error);
+            return NextResponse.json({message:USER_EXISTS_ALREADY});
+        }
     }
-    else{
-        throw new Error("User Exists Already");
-    } 
 
     
 }
