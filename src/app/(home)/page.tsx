cbf160ce0cf5e4208523882c1687e17a607c5935
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-20 12:15:40 +0530

    Add Metadata for the Home Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,13 @@
 import { Features } from "@/components/features"
 import { Footer } from "@/components/footer"
 import { Herocard } from "@/components/heroCard"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Medmatch",
+  description: "Ace your Obstetrics and Gynecology Interviews with our personalized AI Experience",
+}
+
 
 export default function Home() {
   return (
