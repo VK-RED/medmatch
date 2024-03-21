1023e0bd203fa665fc7e8efe5cd0d1597cc4b14d
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-21 13:56:41 +0530

    Configure Prisma for local Development

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
-export default prisma;
+const prismaClientSingleton = () => {
+  console.log("Prisma Client Initialized !!")
+  return new PrismaClient()
+}
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+
+
+if(process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+
+export default prisma
