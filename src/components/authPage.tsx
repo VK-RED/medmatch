3f2f60e88f5115e04081e0ff03f296b2c24fb7bc
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-22 11:05:54 +0530

    Fix the Icon and Quotes in AuthPage

--- src/components/authPage.tsx
+++ src/components/authPage.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils"
 import { UserAuthForm } from "@/components/user-auth-form"
 import { buttonVariants } from "@/components/ui/button"
 import { AuthPageProps, AuthStatus } from "@/lib/types"
+import { MountainIcon } from "lucide-react"
 
 export default function AuthPage({title, description, authstatus}: AuthPageProps) {
 
@@ -20,28 +21,21 @@ export default function AuthPage({title, description, authstatus}: AuthPageProps
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-800" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            {/* TODO: ADD A RELEVANT SVG */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Medmatch
-          </div>
+          <Link href="/">
+            <div className="relative z-20 flex items-center text-lg font-medium">
+              <MountainIcon className="h-6 w-6 dark:animate-pulse mx-2" />
+              <div>
+                Medmatch
+              </div>
+            </div>
+          </Link>
+          
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;Knowledge gives you the real power !.&rdquo;
+                &ldquo;The roots of education are bitter, but the fruit is sweet !&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Aristotle</footer>
             </blockquote>
           </div>
         </div>
