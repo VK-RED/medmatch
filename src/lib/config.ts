d5e95e9a69920be2cc9556ad78ef0787e76e674d
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-25 13:07:05 +0530

    Update the AgentId in Configs

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
-export const agentId = "b41b47654b0b7f15f580df872658a9cc";
+export const agentId = "94e43479867690d36296f5e75ad6d3d6";
 
 export const llmUrl = "https://medmatch-llm-api.vidged.com";
