85842188ac6518678032f1e3a1c38901213b4921
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-30 20:28:15 +0530

    Add State for csvfile in /interview/type Page

--- src/app/(interview)/interview/type/page.tsx
+++ src/app/(interview)/interview/type/page.tsx
@@ -1,11 +1,36 @@
 'use client'
 
 import { CsvDropZone } from "@/components/dropzone"
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react"
 
 export default function InterviewTypePage(){
+
+    const[csvfile,setCsvfile] = useState<File|null>(null);
+
+    useEffect(()=>{
+        console.log(csvfile);
+    },[csvfile])
+
+    const sendFile = async()=>{
+        if(!csvfile) return;
+        const formData = new FormData();
+        formData.append('csvfile',csvfile);
+        const res = await fetch("/api/upload",{
+            method:'POST',
+            body:formData,
+        })
+        console.log(await res.json());
+    }
     
     return (
-        <CsvDropZone />
+        <div>
+            <CsvDropZone setCsvfile={setCsvfile}/>
+            <div>
+                <Button onClick={sendFile}>SEND</Button>
+            </div>
+        </div>
+        
     )
 }
 
