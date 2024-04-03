9a371dd13a6741a433436338650d4217df6d5b7f
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-03 14:36:09 +0530

    Add embedding and chatModel

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,16 @@
 import OpenAI from 'openai';
 
+import { ChatOpenAI } from "@langchain/openai";
+import { OpenAIEmbeddings } from "@langchain/openai";
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const embeddings = new OpenAIEmbeddings();
+
+export const chatModel = new ChatOpenAI({
+  openAIApiKey: OPENAI_API_KEY,
+});
+
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+  apiKey: OPENAI_API_KEY, 
 });
