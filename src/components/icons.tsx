505568e7a604789da9ca9e3d004731db034cc24d
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-07 11:06:09 +0530

    Add CloudIcon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -59,5 +59,8 @@ export const Icons = {
       />
     </svg>
 
+  ),
+  cloudUpload : () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-cloud-upload h-7 w-7"><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"/><path d="M12 12v9"/><path d="m16 16-4-4-4 4"/></svg>
   )
 }
