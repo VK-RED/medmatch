5f7038cf95d684b9cb61fecb0222e54a13bee3e7
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-08 12:08:27 +0530

    Add Toasts and Loaders for Interview Type Page

--- src/app/(interview)/interview/type/page.tsx
+++ src/app/(interview)/interview/type/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 import { DropZone } from "@/components/dropzone"
 import { Fileviewer } from "@/components/fileViewer";
+import { Icons } from "@/components/icons";
+import { TypePageLoader } from "@/components/typePageLoader";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
 
@@ -11,6 +15,16 @@ export default function InterviewTypePage(){
     const router = useRouter();
     const[disabled,setDisabled] = useState(true);
     const[fileName,setFileName] = useState("");
+    const {status} = useSession();
+    const {toast} = useToast();
+    const [loading,setLoading] = useState(false);
+
+    useEffect(()=>{
+        if(status === 'unauthenticated'){
+            router.push("/");
+        }
+    },[status])
+    
 
     useEffect(()=>{
         console.log(file);
@@ -18,6 +32,10 @@ export default function InterviewTypePage(){
 
     const sendFile = async()=>{
         if(!file) return;
+        setLoading((p)=>true);
+        toast({
+            title:"Hang on for a moment !! Our Interviewer is preparing Questions !"
+        })
         const formData = new FormData();
         formData.append('file',file);
         const res = await fetch("/api/upload",{
@@ -25,36 +43,50 @@ export default function InterviewTypePage(){
             body:formData,
         })
         const {chatId,title} = await res.json();
+        setLoading((p)=>false);
         router.push(`/interview/personalised/${title}/${chatId}`)
     }
-    
-    return (
-        <div className="py-20 flex flex-col items-center">
 
-            <div className="flex flex-col items-center space-y-6 mb-20 w-full">
+    if(status === 'loading'){
+        return (<TypePageLoader />)
+    }
+    if(status === 'authenticated'){
+        return (
+            <div className="py-20 flex flex-col items-center">
 
-                <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                    Upload files to get Customised Interview !
-                </h2>
+                <div className="flex flex-col items-center space-y-6 mb-20 w-full">
 
-                <DropZone setFile={setFile} setDisabled={setDisabled} setFileName={setFileName}/>
-                {file && <Fileviewer fileName={fileName} fileUrl={URL.createObjectURL(file)}/>}
-                <Button disabled={disabled} onClick={sendFile}>SEND</Button>
+                    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                        Upload files to get Customised Interview !
+                    </h2>
+
+                    <DropZone setFile={setFile} setDisabled={setDisabled} setFileName={setFileName}/>
+                    {file && <Fileviewer fileName={fileName} fileUrl={URL.createObjectURL(file)}/>}
+                    <Button disabled={disabled||loading} onClick={sendFile}>
+                        {loading && (
+                            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+                        SEND
+                    </Button>
+                </div>
+
+                <div className="border border-b border-dashed border-gray-400 max-w-7xl w-full" />
+
+                <div className="flex flex-col items-center mt-20">
+                    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                        Quick start with General OB-GYN Questions !
+                    </h2>
+                    <p className="text-md text-muted-foreground mb-10">{`Incase you don't have any files. You can go with General OB-GYN interview`}</p>
+                    <Button disabled={loading}
+                        onClick={()=>{
+                        router.push('/interview')
+                    }}>GET STARTED</Button>
+                </div>
+
+                
             </div>
-
-            <div className="border border-b border-dashed border-gray-400 max-w-7xl w-full" />
-
-            <div className="flex flex-col items-center mt-20">
-                <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                    Quick start with General OB-GYN Questions !
-                </h2>
-                <p className="text-md text-muted-foreground mb-10">{`Incase you don't have any files. You can go with General OB-GYN interview`}</p>
-                <Button onClick={()=>{
-                    router.push('/interview')
-                }}>GET STARTED</Button>
-            </div>
-        </div>
-        
-    )
+            
+        )
+    }
 }
 
