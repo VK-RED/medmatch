cff411cecc35061477af56002a45ac9decbddff3
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-15 12:10:37 +0530

    Change Ordering of Interview in Type Page

--- src/app/(interview)/interview/type/page.tsx
+++ src/app/(interview)/interview/type/page.tsx
@@ -65,7 +65,20 @@ export default function InterviewTypePage(){
         return (
             <div className="py-20 flex flex-col items-center">
 
-                <div className="flex flex-col items-center space-y-6 mb-20 w-full">
+                <div className="flex flex-col items-center mb-20">
+                    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                        Quick start with General OB-GYN Questions !
+                    </h2>
+                    <p className="text-md text-muted-foreground mb-10">{`Incase you don't have any files. You can go with General OB-GYN interview`}</p>
+                    <Button disabled={loading}
+                        onClick={()=>{
+                        router.push('/interview')
+                    }}>GET STARTED</Button>
+                </div>
+
+                <div className="border border-b border-dashed border-gray-400 max-w-7xl w-full" />
+
+                <div className="flex flex-col items-center space-y-6 w-full mt-20">
 
                     <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
                         Upload files to get Customised Interview !
@@ -79,19 +92,6 @@ export default function InterviewTypePage(){
                         )}
                         SEND
                     </Button>
-                </div>
-
-                <div className="border border-b border-dashed border-gray-400 max-w-7xl w-full" />
-
-                <div className="flex flex-col items-center mt-20">
-                    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                        Quick start with General OB-GYN Questions !
-                    </h2>
-                    <p className="text-md text-muted-foreground mb-10">{`Incase you don't have any files. You can go with General OB-GYN interview`}</p>
-                    <Button disabled={loading}
-                        onClick={()=>{
-                        router.push('/interview')
-                    }}>GET STARTED</Button>
                 </div>
 
                 
