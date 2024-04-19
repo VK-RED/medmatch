09b1efc64316dc21b4cd0281b9d12ff3a43fc5f5
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-19 09:22:02 +0530

    Update the NEXT_PUBLIC_AGENT_ID in agentId

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
-export const agentId = "09d3e6756d0cf1093f18b2c4a16c6173";
+export const agentId = process.env.NEXT_PUBLIC_AGENT_ID as string;
 
 export const llmUrl = "https://medmatch-llm-api.vidged.com";
