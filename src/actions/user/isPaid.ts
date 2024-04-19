8be4f898504fcb281b00fa04c90a389f455c789c
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-19 10:02:27 +0530

    Create isPaidUser action

---
+++ src/actions/user/isPaid.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+
+export const isPaidUser = async (email:string|null|undefined)=>{
+
+    if(!email){
+        return false;
+    }
+
+    const user = await prisma.user.findUnique(
+        {
+            where:{
+                email
+            },
+            select:{
+                minutesLeft:true
+            }
+        }
+    )
+
+    const minutes = user?.minutesLeft?.minutes || 0;
+
+    if(minutes > 0) return true;
+    return false;
+
+}
