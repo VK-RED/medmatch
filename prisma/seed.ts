59d8ad9bfd6bfbdee5de19b3a4d93eab8e5fbd16
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-21 12:07:18 +0530

    Add Prisma Seed File

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prisma";
+
+async function main(){
+
+    const emailid1 = process.env.EMAILID_1 as string;
+    const emailid2 = process.env.EMAILID_2 as string;
+
+    const userIds = await prisma.user.findMany({
+        where:{
+            email:emailid1 || emailid2,
+        },
+        select:{
+            id:true,
+        }
+    })
+
+    userIds.forEach(async (u)=>{
+        await prisma.minutesLeft.update({
+            where:{
+                userId:u.id
+            },
+            data:{
+                minutes:10000
+            }
+        })
+    })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
