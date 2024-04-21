398180f6c55b57007df3232deec04e0c0123103c
Vishnu Kumar <vishnukumart96@gmail.com>
2024-04-21 12:14:05 +0530

    fix: featureCard

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -9,9 +9,9 @@ export const Features = () => {
             <div className="container space-y-12 px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
-                        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Features</h2>
+                        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">{`Features`}</h2>
                         <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                            Custom exam-level vignettes created by Harvard Med OBGYN Faculty.
+                            {`Custom exam-level vignettes created by Harvard Med OBGYN Faculty.`}
                         </p>
                     </div>
                 </div>
@@ -20,29 +20,29 @@ export const Features = () => {
 
                         <div className='flex items-center gap-3 mx-auto'>
                             <BookOpenCheck />
-                            <h3 className="text-2xl font-bold">AI Interview Simulations</h3>
+                            <h3 className="text-2xl font-bold">{`AI Interview Simulations`}</h3>
                         </div>
                     
                         <p className="text-gray-500 dark:text-gray-400 text-center">
-                            Speak like you're talking to a real human interviewer
+                            {`Speak like you're talking to a real human interviewer`}
                         </p>
                     </div>
                     <div className="flex flex-col justify-center space-y-4">
                         <div className='flex items-center gap-3 mx-auto'>
                             <PackageOpen />
-                            <h3 className="text-2xl font-bold">Get Interviewed on your case list</h3>
+                            <h3 className="text-2xl font-bold">{`Get Interviewed on your case list`}</h3>
                         </div>
                         <p className="text-gray-500 dark:text-gray-400 text-center">
-                            Get Tailored Board-level questions on your case list PDF
+                            {`Get Tailored Board-level questions on your case list PDF`}
                         </p>
                     </div>
                     <div className="flex flex-col justify-center space-y-4">
                         <div className='flex items-center gap-3 mx-auto'>
                             <LibraryBig />
-                            <h3 className="text-2xl font-bold">Get control of your time</h3>
+                            <h3 className="text-2xl font-bold">{`Get control of your time`}</h3>
                         </div>
                         <p className="text-gray-500 dark:text-gray-400 text-center">
-                            Practice at any hour of the day, whenever you have down time
+                            {`Practice at any hour of the day, whenever you have down time`}
                         </p>
                     </div>
                     <div className="flex flex-col justify-center space-y-4">
@@ -51,7 +51,7 @@ export const Features = () => {
                             <h3 className="text-2xl font-bold">Mock Interviews</h3>
                         </div>
                         <p className="text-gray-500 dark:text-gray-400 text-center">
-                            Learn from Harvard Med attending level questions, without worrying about failing in front of a real doctor!
+                            {`Learn from Harvard Med attending level questions, without worrying about failing in front of a real doctor !`}
                         </p>
                     </div>
                 </div>
