14ce1314fe4780c89725efca8f787f5b159e0617
VK-RED <vishnukumart96@gmail.com>
2024-05-13 12:53:55 +0530

    Style Return Card

--- src/app/(payments)/return/page.tsx
+++ src/app/(payments)/return/page.tsx
@@ -1,5 +1,9 @@
 import { Return } from "@/components/Return";
 
 export default function ReturnPage(){
-    return (<Return/>)
+    return (
+        <div className="h-screen w-screen flex flex-col justify-center items-center">
+            <Return/>
+        </div>
+    )
 }
